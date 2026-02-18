61d5339541ff2a6581cd18d6921d06dc6333527c
T4HHH <arunakindavong@gmail.com>
2026-02-18 09:51:06 +0000

    finished test

--- CodePFA/test_integration.c
+++ CodePFA/test_integration.c
@@ -14,6 +14,37 @@ int main()
   double b =;
   int N =;
   double dx =;
-  printf("The integral of th
+  QuadFormula* qf;
+  printf("The integral of the function f(x) in between %d and %d using %i subdivisions is:\n", a , b, N);
+  setQuadFormula(qf,"left")
+  printf("Left : %d\n",double integrate(f, a, b, N, qf));
+  setQuadFormula(qf,"right")
+  printf("Right : %d\n",double integrate(f, a, b, N, qf));
+  setQuadFormula(qf,"middle")
+  printf("Middle : %d\n",double integrate(f, a, b, N, qf));
+  setQuadFormula(qf,"trapezes")
+  printf("Trapezes : %d\n",double integrate(f, a, b, N, qf));
+  setQuadFormula(qf,"simpson")
+  printf("Simpson : %d\n",double integrate(f, a, b, N, qf));
+  setQuadFormula(qf,"gauss2")
+  printf("Gauss2 : %d\n",double integrate(f, a, b, N, qf));
+  setQuadFormula(qf,"gauss3")
+  printf("Gauss3 : %d\n",double integrate(f, a, b, N, qf));
+
+  printf("The integral of the function f(x) in between %d and %d using subdivisions of %d length is:\n", a , b, dx);
+  setQuadFormula(qf,"left")
+  printf("Left : %d\n",double integrate_dx(f, a, b, dx, qf));
+  setQuadFormula(qf,"right")
+  printf("Right : %d\n",double integrate_dx(f, a, b, dx, qf));
+  setQuadFormula(qf,"middle")
+  printf("Middle : %d\n",double integrate_dx(f, a, b, dx, qf));
+  setQuadFormula(qf,"trapezes")
+  printf("Trapezes : %d\n",double integrate_dx(f, a, b, dx, qf));
+  setQuadFormula(qf,"simpson")
+  printf("Simpson : %d\n",double integrate_dx(f, a, b,dx, qf));
+  setQuadFormula(qf,"gauss2")
+  printf("Gauss2 : %d\n",double integrate_dx(f, a, b, dx, qf));
+  setQuadFormula(qf,"gauss3")
+  printf("Gauss3 : %d\n",double integrate_dx(f, a, b, dx, qf));
   return 0;
 }
